7a2ca4a9147697ba701c87219f0fe897b12efb0f
xmbys <1498790327@qq.com>
2018-09-30 15:01:14 +0800

    second can play

--- iic.c
+++ iic.c
@@ -4,12 +4,16 @@
 # include "iic.h"
 # include "sys.h"
 # include "delay.h"
+# include "usart.h"
+
 void iic_delay()
 {
+	
 	int i=72;
 	while(i--);
 		
 }
+
 void iic_init_out()
 {
 	//delay_init(9000);
@@ -17,26 +21,28 @@ void iic_init_out()
 	
 	GPIO_InitTypeDef gpio_iic;
 	gpio_iic.GPIO_Pin = IIC_SDA_ping|IIC_SCL_ping;
-	gpio_iic.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio_iic.GPIO_Speed = GPIO_Speed_2MHz;
 	gpio_iic.GPIO_OType= GPIO_OType_PP;                                   
-	gpio_iic.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//	gpio_iic.GPIO_PuPd = GPIO_PuPd_UP;
+	gpio_iic.GPIO_PuPd=GPIO_PuPd_UP;
 	gpio_iic.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_Init(IIC_GPIO_what,&gpio_iic);
 	
-	IIC_SCL_out=1;
-	iic_delay();
-	IIC_SDA_out=1;
-	iic_delay();
-	
-	IIC_SCL_out=0;
-	iic_delay();
-	IIC_SDA_out=0;
-	iic_delay();
+//	IIC_SCL_out=1;
+//	iic_delay();
+//	IIC_SDA_out=1;
+//	iic_delay();
+//	
+//	IIC_SCL_out=0;
+//	iic_delay();
+//	IIC_SDA_out=0;
+//	iic_delay();
 
 }
 
 void iic_init_in()
 {
+	RCC_AHB1PeriphClockCmd(IIC_GPIO_RCC,ENABLE);
 	GPIO_InitTypeDef gpio_iic;
 	gpio_iic.GPIO_Pin = IIC_SDA_ping|IIC_SCL_ping;
 	gpio_iic.GPIO_Speed = GPIO_Speed_50MHz;
@@ -46,84 +52,165 @@ void iic_init_in()
 	GPIO_Init(IIC_GPIO_what,&gpio_iic);
 }
 
-void iic_start()                            //iic开始信号
+void iicSCL_init_in()
 {
-	IIC_SCL_out=0;
-	iic_delay();
-	IIC_SDA_out=1;
-	iic_delay();
-	IIC_SCL_out=1;
-	iic_delay();
-	IIC_SDA_out=0;
-	iic_delay();
-	IIC_SCL_out=0;
-	iic_delay();
+	RCC_AHB1PeriphClockCmd(IIC_GPIO_RCC,ENABLE);
+	GPIO_InitTypeDef gpio_iic;
+	gpio_iic.GPIO_Pin = IIC_SCL_ping;
+	gpio_iic.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio_iic.GPIO_OType= GPIO_OType_PP;
+	gpio_iic.GPIO_PuPd = GPIO_PuPd_UP;
+	gpio_iic.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Init(IIC_GPIO_what,&gpio_iic);
+}
+void iicSDA_init_in()
+{
+	RCC_AHB1PeriphClockCmd(IIC_GPIO_RCC,ENABLE);
+	GPIO_InitTypeDef gpio_iic;
+	gpio_iic.GPIO_Pin = IIC_SDA_ping;
+	gpio_iic.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio_iic.GPIO_OType= GPIO_OType_PP;
+	gpio_iic.GPIO_PuPd = GPIO_PuPd_UP;
+	gpio_iic.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Init(IIC_GPIO_what,&gpio_iic);
+}
+void iicSCL_init_out()
+{
+	RCC_AHB1PeriphClockCmd(IIC_GPIO_RCC,ENABLE);
+	GPIO_InitTypeDef gpio_iic;
+	gpio_iic.GPIO_Pin = IIC_SCL_ping;
+	gpio_iic.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio_iic.GPIO_OType= GPIO_OType_PP;
+	gpio_iic.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	gpio_iic.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(IIC_GPIO_what,&gpio_iic);
+}
+
+void iicSDA_init_out()
+{
+	//delay_init(9000);
+	RCC_AHB1PeriphClockCmd(IIC_GPIO_RCC,ENABLE);
+	
+	GPIO_InitTypeDef gpio_iic;
+	gpio_iic.GPIO_Pin = IIC_SDA_ping;
+	gpio_iic.GPIO_Speed = GPIO_Speed_2MHz;
+	gpio_iic.GPIO_OType= GPIO_OType_PP;                                   
+	gpio_iic.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	gpio_iic.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(IIC_GPIO_what,&gpio_iic);
+	
+
+}
+
+
+int iic_start()                            //iic开始信号
+{
+	
+	SCL_L;iic_delay();//while((GPIOB->ODR  & GPIO_Pin_7)==0);
+	
+	SDA_H;iic_delay();
+	
+	SCL_H;iic_delay();
+	
+	if((SCL_read)==0){return 0;} iic_delay();
+	
+	SDA_L;iic_delay();
+	
+	SCL_L;iic_delay();
+	
+	return 1;
+	
 }
 
 void iic_stop()
 {
-	IIC_SDA_out=0;
-	iic_delay();
-	IIC_SCL_out=1;
-	iic_delay();
-	IIC_SDA_out=1;
-	iic_delay();
-	IIC_SCL_out=0;
-	iic_delay();
+  SDA_L;iic_delay();
+	
+	SCL_H;iic_delay();
+	
+	SDA_H;iic_delay();
+	
+	SCL_L;iic_delay();
 	
 }
 
-void iic_ack()
+void iic_ack()      //应答信号
 {
 	int i=0;
-	IIC_SCL_out=0;
-	iic_delay();
-	IIC_SDA_out=1;
-	iic_delay();
-	IIC_SCL_out=1;
-//	while((IIC_SDA_in==0)&&i<255)
-//	{
-//		i++;
-//	}
-	iic_delay();
-	IIC_SDA_out=0;
-	iic_delay();
-	IIC_SCL_out=0;
-	iic_delay();
+	
+	SCL_L;iic_delay();
+	
+	SDA_L;iic_delay();
+	
+	SCL_H;iic_delay();
+	
+  SCL_L;iic_delay();
+	
+	SCL_L;iic_delay();
+	
 }
 
-void iic_waitack()
+void iic_noack()
+{
+	
+	SCL_L;iic_delay();
+	
+	SDA_H;iic_delay();
+	
+	SCL_H;iic_delay();
+	
+	SCL_L;iic_delay();
+	
+}
+
+int iic_waitack()
 {
 	int i;
-	IIC_SCL_out=0;
-	iic_delay();
-	IIC_SDA_out=1;
-	iic_delay();
-IIC_SCL_out=1;
-	while(((IIC_SDA_out)==1)&&i<255)
-		i++;
 	
-	iic_delay();
+	SCL_L;iic_delay();
+	
+	SDA_H;iic_delay();
+	
+	SCL_H;iic_delay();
+  
+	if(SDA_read)
+	{
+		
+		SCL_L;iic_delay();
+		
+		return 0;
+	}
+	
+	SCL_L;iic_delay();
+	
+	return 1;
 }
 
 void IIC_write_bit(unsigned char num)            //写一个字节
 {
 	unsigned char i,j=0;
-	IIC_SDA_out=0;
+	unsigned char k;
+	k=num;
+	SCL_L;iic_delay();
+	SDA_L;iic_delay();
 	for(i=0;i<8;i++)
 	{
-		j=0x80&num;
+		j=0x80&k;
+		
 		if(j!=0)
-		    IIC_SDA_out=1;
- 		IIC_SCL_out=1;
-		iic_delay();
-
-		iic_delay();
-		IIC_SCL_out=0;
-		iic_delay();
-		IIC_SDA_out=0;
-		iic_delay();
-		num=num<<1;
+			
+ 		      {SDA_H;iic_delay();}
+		
+		if (j==0)
+			
+			    {SDA_L;iic_delay();}
+		
+		SCL_H;iic_delay();
+		
+		SCL_L;iic_delay();
+		
+		k=k<<1;
+		
 	}
 	
 }
@@ -131,38 +218,67 @@ void IIC_write_bit(unsigned char num)            //写一个字节
 unsigned  char IIC_read_bit()    //读一个字节-
 {
 	unsigned char i,j=0,k=0;
-	IIC_SDA_out=1;iic_delay();
+	
+	SCL_L;iic_delay();
+  SDA_H;iic_delay();
 	for(i=0;i<8;i++)
+	
 	{ 
-		IIC_SCL_out=1;
-		iic_delay();
-		k=IIC_SDA_in;
-		j=(j<<1)|k;
-	  IIC_SCL_out=0;
-		iic_delay();
+		
+		SCL_H;iic_delay();
+		
+		k=SDA_read;iic_delay();
+		
+		j=(j<<1);
+		
+		if(k!=0){j=j|1;}
+		
+		SCL_L;iic_delay();
+		
 	}
+	
+//	    iicSDA_init_out();iic_delay();
+	
 	return j;
 }
 
 void IIC_WrRigist(unsigned char  addr,unsigned char num)    //往一个寄存器写一个字节
 {
+	
 	iic_start();
+	
 	IIC_write_bit(0x68<<1);iic_waitack();
+	
 	IIC_write_bit(addr);iic_waitack();
+	
 	IIC_write_bit(num); iic_waitack();
+	
 	iic_stop();
+	
 }
 
 unsigned char IIC_ReRigist(unsigned char addr)             //从一个寄存器读一个字节
 {
-	unsigned num;
-	iic_start();
-	IIC_write_bit(0x68<<1);iic_waitack();
+	unsigned char num=0;
+	
+	int k;
+	
+	iic_start();//printf("%d ",k);
+	
+	IIC_write_bit((0x68)<<1);iic_waitack();
+	
 	IIC_write_bit(addr);iic_waitack();
+	
 	iic_start();
-	IIC_write_bit(0x68*2+0x01);iic_waitack();
-	num=IIC_read_bit();iic_ack();
+	
+	IIC_write_bit((0x68)<<1|0x01);iic_waitack();
+	
+	num=IIC_read_bit();//iic_ack();
+//	USART_SendData(UART4,num);
+	
+//	iic_noack();
 	iic_stop();
+	
 	return num;
 }
 
--- iic.h
+++ iic.h
@@ -9,6 +9,18 @@
 # define IIC_SCL_out   PBout(6)
 # define IIC_SDA_in    PBin(7)
 # define IIC_SCL_in    PBin(6)
+//************************************
+/*模拟IIC端口输出输入定义*/
+#define SCL_L         GPIOB->BSRRH = GPIO_Pin_6  //;GPIOB->BSRRL  = 0
+#define SCL_H         GPIOB->BSRRL = GPIO_Pin_6  //;GPIOB->BSRRH = 0
+   
+#define SDA_L         GPIOB->BSRRH = GPIO_Pin_7   //;GPIOB->BSRRL  = 0
+#define SDA_H         GPIOB->BSRRL = GPIO_Pin_7  //;GPIOB->BSRRH = 0
+
+#define SCL_read      GPIOB->IDR  & GPIO_Pin_6
+#define SDA_read      GPIOB->IDR  & GPIO_Pin_7
+
+
 
 void iic_init_out();
 void IIC_WrRigist(unsigned char  addr,unsigned char num);
